041b81f56b541e9442629f6ed8375a173ecb67e1
Tareq <tareqnmd@gmail.com>
2024-02-13 11:47:00 +0600

    palindrome

--- data-structure-interview-questions/palindrome.js
+++ data-structure-interview-questions/palindrome.js
@@ -5,10 +5,16 @@
 // Input: x = 10   ----->>>>>   Output: false
 
 var isPalindrome = function (x) {
-  return x < 0 ? false : x === +x.toString().split("").reverse().join("");
+	return x < 0 ? false : x === +x.toString().split('').reverse().join('');
+};
+
+const myPalindrome = (x) => {
+	return String(x) === String(x).split('').reverse().join('');
 };
 
 const res = isPalindrome(10);
-console.log(res);
+const res2 = myPalindrome(11);
+const res3 = myPalindrome(12);
+console.log(res, res2, res3);
 
 // 121 => "121" => ["1","2","1"] => ["1","2","1"] => "121"
