3a7fa1c485142abbfb65a907e7d2bd885fa6161b
Tareq <tareqnmd@gmail.com>
2024-02-13 13:59:06 +0600

    up

--- data-structure-interview-questions/palindrome.js
+++ data-structure-interview-questions/palindrome.js
@@ -1,10 +1,4 @@
-// Ques 1 - Palindrome Number
-// An integer is a palindrome when it reads the same forward and backward.
-
-// Input: x = 121  ----->>>>>   Output: true
-// Input: x = 10   ----->>>>>   Output: false
-
-var isPalindrome = function (x) {
+var basePalindrome = function (x) {
 	return x < 0 ? false : x === +x.toString().split('').reverse().join('');
 };
 
@@ -12,9 +6,48 @@ const myPalindrome = (x) => {
 	return String(x) === String(x).split('').reverse().join('');
 };
 
-const res = isPalindrome(10);
-const res2 = myPalindrome(11);
-const res3 = myPalindrome(12);
-console.log(res, res2, res3);
+function palindromeEvery(str) {
+	return str
+		.split('')
+		.every((char, index) => char === str[str.length - 1 - index]);
+}
 
-// 121 => "121" => ["1","2","1"] => ["1","2","1"] => "121"
+const palindromeOptimize = (x) => {
+	if (x < 0) return false;
+	const str = String(x);
+	for (let i = 0; i < parseInt(str.length / 2); i++) {
+		if (str[i] !== str[str.length - 1 - i]) return false;
+	}
+	return true;
+};
+
+const isPalindrome = (s) => {
+	const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+	for (let i = 0; i < parseInt(str.length / 2); i++) {
+		if (str[i] !== str[str.length - 1 - i]) return false;
+	}
+	return true;
+};
+
+var linkedPalindrome = function (head) {
+	for (let i = 0; i < parseInt(head.length / 2); i++) {
+		console.log(
+			head[i],
+			head[head.length - 1 - i],
+			head[i] !== head[head.length - 1 - i]
+		);
+		if (head[i] !== head[head.length - 1 - i]) return false;
+	}
+	return true;
+	// return head.join('') === head.reverse().join('');
+};
+
+var firstPalindrome = function (head) {
+	for (let i = 0; i < head.length; i++) {
+		if (isPalindrome(head[i])) return head[i];
+	}
+	return '';
+};
+
+const res = linkedPalindrome([1, 2]);
+console.log(res);
