2189281bf4573948f866739eed196dde13e30c8d
Tareq <tareqnmd@gmail.com>
2024-03-03 15:52:53 +0600

    up

--- password-generator/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ onClick, text, customClass }) => {
-  return (
-    <button className={customClass} onClick={onClick}>
-      {text}
-    </button>
-  );
-};
-
-export default Button;
--- progress-bar/src/constants.js
+++
@@ -1,2 +0,0 @@
-export const MIN = 0;
-export const MAX = 100;
