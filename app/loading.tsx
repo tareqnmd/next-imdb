da2b108c6676d95f2b22d7ad7d2025ddff630ae0
Tareq <tareqnmd@gmail.com>
2024-03-04 18:36:28 +0600

    up

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,9 @@
-import Image from 'next/image';
+import { BiLoader } from 'react-icons/bi';
 
 export default function loading() {
 	return (
 		<div className="flex justify-center mt-16">
-			<Image
-				className="h-52"
-				src="spinner.svg"
-				width={100}
-				height={100}
-				alt="loading..."
-			/>
+			<BiLoader />
 		</div>
 	);
 }
