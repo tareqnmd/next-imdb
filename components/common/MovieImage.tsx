81513b97c0990ed5f607b43e9aec4aa19ede4f43
Tareq <tareqnmd@gmail.com>
2024-03-09 13:17:26 +0600

    blur

--- components/common/MovieImage.tsx
+++ components/common/MovieImage.tsx
@@ -4,14 +4,15 @@ import Image from 'next/image';
 const MovieImage = ({ image, title, extraClasses = '' }: any) => {
 	return (
 		<Image
-			loader={() => '/default.jpg'}
 			src={
 				image ? `https://image.tmdb.org/t/p/original/${image}` : '/default.jpg'
 			}
 			width="0"
 			height="0"
+			loading="lazy"
 			sizes="100vw"
-			className={`overflow-hidden w-full ${extraClasses}`}
+			className={`overflow-hidden w-full transition-opacity opacity-0 duration-[0.5s] ${extraClasses}`}
+			onLoadingComplete={(img) => img.classList.remove('opacity-0')}
 			alt={title}
 		></Image>
 	);
